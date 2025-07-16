cdb96d22863380c0999cae9d950354122690857b
vuvantoann <toanvu1728@gamil.com>
2025-07-16 15:33:21 +0700

    Lấy ra danh sách công việc

--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-console.log('OKE')
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import * as database from './config/database'
+import Task from './models/task.model'
+
+dotenv.config()
+database.connect()
+const app: Express = express()
+const port: string | number = process.env.PORT || 3000
+
+app.get('/', async (req: Request, res: Response) => {
+  const tasks = await Task.find({
+    deleted: false,
+  })
+
+  res.json({
+    data: 200,
+    tasks: tasks,
+  })
+})
+
+app.get('/tasks/detail/:id', async (req: Request, res: Response) => {
+  const id: string = req.params.id
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false,
+  })
+
+  res.json({
+    data: 200,
+    tasks: task,
+  })
+})
+
+app.listen(port, () => {
+  console.log('lắng nghe thành công ')
+})
