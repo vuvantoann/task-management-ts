6171b7bf85a8c8eaa42486dd0180a75d021b8761
vuvantoann <toanvu1728@gamil.com>
2025-07-17 10:49:15 +0700

    Bộ lọc theo trạng thái

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,21 @@ import { Request, Response } from 'express'
 import Task from '../models/task.model'
 
 export const index = async (req: Request, res: Response) => {
-  const tasks = await Task.find({
+  interface Find {
+    deleted: boolean
+    status?: string
+  }
+
+  const find: Find = {
     deleted: false,
-  })
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status.toString()
+  }
+
+  const tasks = await Task.find(find)
+
   res.json(tasks)
 }
 export const detail = async (req: Request, res: Response) => {
