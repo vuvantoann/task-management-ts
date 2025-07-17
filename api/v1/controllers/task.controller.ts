0474d6c47b9487ef7af8cc023007e076074da56c
vuvantoann <toanvu1728@gamil.com>
2025-07-17 11:43:27 +0700

    Sắp xếp theo tiêu chí

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import Task from '../models/task.model'
 
 export const index = async (req: Request, res: Response) => {
+  // lọc theo trạng thái
   interface Find {
     deleted: boolean
     status?: string
@@ -14,8 +15,15 @@ export const index = async (req: Request, res: Response) => {
   if (req.query.status) {
     find.status = req.query.status.toString()
   }
+  // kết thúc
 
-  const tasks = await Task.find(find)
+  // sắp xếp theo tiêu chí
+  let sort = {}
+  if (req.query.sortKey && req.query.sortValue) {
+    const sortKey = req.query.sortKey.toString()
+    sort[sortKey] = req.query.sortValue
+  }
+  const tasks = await Task.find(find).sort(sort)
 
   res.json(tasks)
 }
