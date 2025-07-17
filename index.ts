efad2a09e7f30e4e01f36c9fef18d7b6fd521ea9
vuvantoann <toanvu1728@gamil.com>
2025-07-17 17:06:00 +0700

    Cài đặt CORS

--- index.ts
+++ index.ts
@@ -2,11 +2,13 @@ import express, { Express } from 'express'
 import dotenv from 'dotenv'
 import * as database from './config/database'
 import routesV1 from './api/v1/routes/index.route'
-
+import cors from 'cors'
 dotenv.config()
 database.connect()
 const app: Express = express()
 const port: string | number = process.env.PORT || 3000
+// Enable CORS
+app.use(cors())
 
 app.use(express.json()) // Thêm dòng này
 //app.use(express.urlencoded({ extended: true }))
